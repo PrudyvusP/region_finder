b4e16be970e142d7d49dabb2e4508a97d4f687b9
PrudyvusP <vgolosyandriya@yandex.ru>
2024-09-24 14:02:12 +0300

    added RegionFinderWithKV release (#5)

--- region_finder/__init__.py
+++ region_finder/__init__.py
@@ -1,3 +1,7 @@
+from .rfinder_alch_kv import RegionDataGetter, RegionFinderWithKV
 from .rfinder_with_sqladb import RegionFinderWithSQLADB
 
-__all__ = [RegionFinderWithSQLADB, ]
+__all__ = [
+    RegionFinderWithKV,
+    RegionFinderWithSQLADB,
+]
